09d9688fd8e489e4c94544b358e5276b0236c25b
chenfangming <chenfangming@91zdan.com>
2017-09-26 18:12:03 +0800

    ToggleButton

---
+++ src/main/java/me/geemu/chapter5_UI/toggle/ToggleChapter.java
@@ -0,0 +1,22 @@
+package me.geemu.chapter5_UI.toggle;
+/**
+ * Author: 陈方明
+ * Email: cfmmail@sina.com
+ * Date: 2017/9/26 17:32
+ * Description:
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ToggleChapter extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
