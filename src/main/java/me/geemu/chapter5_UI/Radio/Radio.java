1847fd99c78f802926dc5eba7b1f31436cc105a6
chenfangming <chenfangming@91zdan.com>
2017-09-26 17:21:08 +0800

    radio

--- src/main/java/me/geemu/chapter5_UI/Radio/Radio.java
+++ src/main/java/me/geemu/chapter5_UI/Radio/Radio.java
@@ -1,4 +1,4 @@
-package me.geemu.chapter5_UI.Radio;
+package me.geemu.chapter5_UI.radio;
 
 import javafx.application.Application;
 import javafx.beans.value.ObservableValue;
@@ -22,39 +22,50 @@ public class Radio extends Application {
     @Override
     public void start(Stage primaryStage) {
         Scene scene = new Scene(new Group());
-        primaryStage.setTitle("Radio Button");
+        primaryStage.setTitle("Radio");
         primaryStage.setWidth(250);
-        primaryStage.setHeight(150);
+        primaryStage.setHeight(200);
 
         final ToggleGroup group = new ToggleGroup();
+        // home
+        RadioButton home = new RadioButton("Home");
+        home.setToggleGroup(group);
+        home.setUserData("Home");
 
-        RadioButton rb1 = new RadioButton("Home");
-        rb1.setToggleGroup(group);
-        rb1.setUserData("Home");
+        // calendar
+        RadioButton calendar = new RadioButton("Calendar");
+        calendar.setToggleGroup(group);
+        calendar.setUserData("Calendar");
 
-        RadioButton rb2 = new RadioButton("Calendar");
-        rb2.setToggleGroup(group);
-        rb2.setUserData("Calendar");
+        // contacts
+        RadioButton contacts = new RadioButton("Contacts");
+        contacts.setToggleGroup(group);
+        contacts.setUserData("Contacts");
 
-        RadioButton rb3 = new RadioButton("Contacts");
-        rb3.setToggleGroup(group);
-        rb3.setUserData("Contacts");
+        // 创建一个带图像的Radio Button
+        Image image = new Image(getClass().getResourceAsStream("/chapter5/radio/Ok.jpg"));
+        RadioButton imageRadio = new RadioButton("Agree");
+        imageRadio.setGraphic(new ImageView(image));
+        imageRadio.setToggleGroup(group);
+        imageRadio.setUserData("Ok");
 
         group.selectedToggleProperty().addListener(
-            (ObservableValue<? extends Toggle> ov, Toggle old_toggle, 
-            Toggle new_toggle) -> {
+            (ObservableValue<? extends Toggle> ov, Toggle old_toggle,Toggle new_toggle) -> {
                 if (group.getSelectedToggle() != null) {
-                    final Image image = new Image(getClass().getResourceAsStream("/chapter5/radio/"+group.getSelectedToggle().getUserData().toString() + ".jpg"));
-                icon.setImage(image);
+                    final Image iconImage = new Image(getClass().getResourceAsStream("/chapter5/radio/"+group.getSelectedToggle().getUserData().toString() + ".jpg"));
+                icon.setImage(iconImage);
             }
         });
-        
+        // 默认home选中
+        home.requestFocus();
+        home.setSelected(true);
         HBox hbox = new HBox();
         VBox vbox = new VBox();
 
-        vbox.getChildren().add(rb1);
-        vbox.getChildren().add(rb2);
-        vbox.getChildren().add(rb3);
+        vbox.getChildren().add(home);
+        vbox.getChildren().add(calendar);
+        vbox.getChildren().add(contacts);
+        vbox.getChildren().add(imageRadio);
         vbox.setSpacing(10);
 
         hbox.getChildren().add(vbox);
@@ -65,5 +76,6 @@ public class Radio extends Application {
         ((Group) scene.getRoot()).getChildren().add(hbox);
         primaryStage.setScene(scene);
         primaryStage.show();
+
     }
 }
